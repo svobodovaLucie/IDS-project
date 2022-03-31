ea3bacc4b18163017d58df4113eb569ad24aab36
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2022-03-31 21:08:54 +0200

    unnecessary selects deleted

--- xsvobo1x_xkuzni04.sql
+++ xsvobo1x_xkuzni04.sql
@@ -325,26 +325,6 @@ INSERT INTO Platba (ID_zamestnanec, ID_objednavka, castka, typ_platby)
 VALUES (2, 3, 256, 'hotovost');
 
 
-/* ************* ZOBRAZENÍ VYTVOŘENÝCH TABULEK NAPLNĚNÝCH UKÁZKOVÝMI DATY ************* */
-
-SELECT * FROM Mistnost;
-SELECT * FROM Stul;
-SELECT * FROM Zakaznik;
-SELECT * FROM Pozice;
-SELECT * FROM Zamestnanec;
-SELECT * FROM Telefon;
-SELECT * FROM Ingredience;
-SELECT * FROM Alergen;
-SELECT * FROM Ingredience_obsahuje_alergen;
-SELECT * FROM Pokrm_napoj;
-SELECT * FROM Ingredience_v_pokrmu_napoji;
-SELECT * FROM Rezervace;
-SELECT * FROM Rezervace_stolu;
-SELECT * FROM Objednavka;
-SELECT * FROM Objednavka_obsahuje_pokrm_napoj;
-SELECT * FROM Platba;
-
-
 /* ************************************** DOTAZY ************************************** */
 
 /* Dotaz zobrazí alergeny obsažené v jednotlivých pokrmech/nápojích. *
@@ -378,33 +358,27 @@ AND NOT EXISTS (SELECT *
                 WHERE Z.ID_zakaznik = R.ID_zakaznik);
   
 /* Dotaz zobrazí veškeré telefony jednotlivých zaměstnanců, kteří jsou kuchaři *
- * - Spojení většího počtu tabulek */
+ * - spojení tří tabulek                                                       */
 SELECT Z.jmeno, Z.prijmeni, TE.telefon, Po.nazev_pozice
 FROM Zamestnanec Z, Telefon TE, Pozice Po
 WHERE Z.zkratka_pozice='kuch' AND Po.zkratka_pozice = Z.zkratka_pozice;
 
-/* Dotaz zobrazí jméno a příjmení zaměstnance, který provedl rezervaci v určíté datum. *
- * - Spojení dvou tabulek */
+/* Dotaz zobrazí jméno a příjmení zaměstnance, který provedl rezervaci dne 27. 3. 2022. *
+ * - spojení dvou tabulek                                                               */
 SELECT R.ID_rezervace, Z.jmeno, Z.prijmeni, R.datum_rezervace
 FROM Zamestnanec Z, Rezervace R 
 WHERE Z.ID_zamestnanec = R.ID_zamestnanec AND R.datum_rezervace='27-MAR-2022';
 
 /* Dotaz zobrazí veškeré pokrmy, které obsahují alergen VEJCE nebo MLEKO *
- * - použití predikátu IN */
+ * - použití predikátu IN                                                */
 SELECT P.nazev, A.nazev
 FROM Pokrm_napoj P, Ingredience_v_pokrmu_napoji IvP, Ingredience_obsahuje_alergen IoA, Alergen A
 WHERE IvP.ID_ingredience = IoA.ID_ingredience AND IoA.ID_alergen = A.ID_alergen
         AND P.ID_pokrm_napoj = IvP.ID_pokrm_napoj AND 
         A.nazev IN ('VEJCE', 'MLÉKO');
 
-/* Dotaz zobrazí veškeré stoly, které nejsou pro 2, nebo 4 osoby a jejich jsou místnosti*
- * - Spojení více tabulek, použití predikatu IN */
+/* Dotaz zobrazí veškeré stoly, které nejsou pro 2 ani 4 osoby a jejich místnosti*
+ * - spojení více tabulek, použití predikatu IN */
 SELECT S.cislo_stolu, S.pocet_mist, S.cislo_mistnosti ,M.nazev 
 FROM Stul S, Mistnost M
 WHERE S.cislo_mistnosti = M.cislo_mistnosti AND S.pocet_mist NOT IN (2,4);
-
-?/
-
-
-/* Dotaz vypočte celkovou cenu jednotlivých objednávek */
--- later ve složitějších úkolech Lux
